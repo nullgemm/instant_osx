b9a7bfab47e4a8350b60ea6bb1397487caa25221
nullgemm <nullgemm@mailbox.org>
2020-02-08 17:18:03 +0100

    fix options order

--- extra_make.sh
+++ extra_make.sh
@@ -3,7 +3,7 @@
 modprobe nbd
 sudo qemu-nbd -c /dev/nbd0 ./macOS-Simple-KVM-master/disk_extra.qcow2
 
-sudo parted -a optimal --script /dev/nbd0\
+sudo parted -s -a optimal -- /dev/nbd0\
 	mklabel msdos\
 	mkpart primary fat32 0% 100%\
 
