8ad594db74fe684a39052e43a754d6bfa1f71f8a
nullgemm <nullgemm@mailbox.org>
2020-02-13 18:56:17 +0100

    fix extra disk tools

--- extra_mount.sh
+++ extra_mount.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-modprobe nbd
+sudo modprobe nbd
 sudo qemu-nbd -c /dev/nbd0 ./macOS-Simple-KVM-master/disk_extra.qcow2
-mkdir ./mnt
-sudo mount /dev/nbd0p1 ./mnt
--- extra_umount.sh
+++ extra_umount.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-sudo umount /dev/nbd0p1
-rmdir ./mnt
 sudo qemu-nbd -d /dev/nbd0
-rmmod nbd
+sudo rmmod nbd
